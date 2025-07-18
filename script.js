db9be6faf0d0ca2152f94710a8298700ce5bacfb
RAFAEL KRUGER VIDAL DA SILVA <rafael.10970@aluno.pr.senac.br>
2025-07-18 17:27:17 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+function gitAzul() {
+  const preto = document.getElementById("git");
+  preto.src = "github-color-svgrepo-com.svg";
+}
+function gitPreto() {
+  const azul = document.getElementById("git");
+  azul.src = "github-142-svgrepo-com.svg";
+}
+function instaCor() {
+  const preto = document.getElementById("insta");
+  preto.src = "instagram-2016-logo.svg";
+}
+function instaPreto() {
+  const cor = document.getElementById("insta");
+  cor.src = "instagram-svgrepo-com.svg";
+}
